10f9569281aba8c39ebd7aabfce94fcaa6ed02d5
Aleksandr Leontev <web.alex.leontev@gmail.com>
2020-01-17 18:12:55 +0300

    Update texttest_fixture.rb

--- ruby/texttest_fixture.rb
+++ ruby/texttest_fixture.rb
@@ -2,18 +2,19 @@
 
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
-puts "OMGHAI!"
+puts "Start test!"
+
 items = [
-  Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
-  Item.new(name="Aged Brie", sell_in=2, quality=0),
-  Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
-  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+  RoseItem.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
+  RoseItem.new(name="Aged Brie", sell_in=2, quality=0),
+  RoseItem.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
+  RoseItem.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+  RoseItem.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+  RoseItem.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+  RoseItem.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+  RoseItem.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
   # This Conjured item does not work properly yet
-  Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+  RoseItem.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
 ]
 
 days = 2
